89c01e45eea7d1720a8484f83bb8d164dca39d14
unknown <you@example.com>
2024-08-21 12:02:25 +0200

    bla

--- cartymode/nullspot.py
+++ cartymode/nullspot.py
@@ -1,8 +1,8 @@
-def start_playback():
+def start_playback(*args, **kwargs):
     print(f"Start fake playback")
 
-def pause_playback(): 
+def pause_playback(*args, **kwargs): 
     print("pause fake playback")
 
-def seek_track(foo):
+def seek_track(*args, **kwargs):
     print("Sake seek playback")
